554d919e5e6fcb3c1f28191e0b26bfd7aa5ca6ee
Harshit Thakur <harshitthakur@ternaengg.ac.in>
2023-01-27 10:32:28 +0530

    level 2 with environment vars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express=require("express");
 const body=require("body-parser");
 const ejs=require("ejs");
@@ -13,8 +14,8 @@ const userSchema=new mongoose.Schema
     password:String
 });
 
-const secret="thisisoursecret.";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const app=express();
 const User=mongoose.model("user",userSchema);
